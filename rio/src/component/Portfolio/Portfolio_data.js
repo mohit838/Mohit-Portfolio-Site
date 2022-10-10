54a5030244af5d462c012ae1e4a00bec59c58945
Mohammad Mohitul Islam <mohitul449@gmail.com>
2022-10-10 17:02:09 +0600

    Update Img

--- rio/src/component/Portfolio/Portfolio_data.js
+++ rio/src/component/Portfolio/Portfolio_data.js
@@ -4,21 +4,21 @@ const Portfolio_data = [
     category: "LANDING PAGE",
     totalLike: "600",
     title: "The services provide for landing page design ",
-    image: "./../pic/protfolioImg/Langing Page.PNG",
+    image: "https://i.ibb.co/5GrYJt4/Langing-Page.png",
   },
   {
     id: 2,
     category: "PSD to HTML",
     totalLike: "750",
     title: "Dine Out Restuarant",
-    image: "./../pic/protfolioImg/PSD to HTML and CSS.PNG",
+    image: "https://i.ibb.co/4mXBVp6/PSD-to-HTML-and-CSS.png",
   },
   {
     id: 3,
     category: "FullStack MERN",
     totalLike: "630",
     title: "Doctors Portal for online appointmennts",
-    image: "./../pic/protfolioImg/Doctors Portal.PNG",
+    image: "https://i.ibb.co/ZhJWFXs/Doctors-Portal.png",
   },
 ];
 export default Portfolio_data;
